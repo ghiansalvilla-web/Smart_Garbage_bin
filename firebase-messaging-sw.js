9bb8055ac5245e20f17e4b418d8e7f03d0b09bb3
ghiansalvilla-web <ghiansalvilla@gmail.com>
2026-05-08 11:00:06 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,16 +4,17 @@ importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-comp
 firebase.initializeApp({
   apiKey: "AIzaSyBdmxDRFTyU05XKGZaHaXwcHp-nuwBjVR0",
   databaseURL: "https://smartgarbagebin-996d0-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "smartgarbagebin-996d0",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  messagingSenderId: "413668818423",  // get this from Firebase console
+  appId: "1:413668818423:web:9464b7cd8b074bd01102f2"                  // get this from Firebase console
 });
 
 const messaging = firebase.messaging();
 
+// Handle background notifications
 messaging.onBackgroundMessage(payload => {
-  self.registration.showNotification(payload.notification.title, {
-    body: payload.notification.body,
+  const { title, body } = payload.notification;
+  self.registration.showNotification(title, {
+    body: body,
     icon: '/icon.png',
     badge: '/icon.png',
     vibrate: [200, 100, 200]
